67482f1953f7f13ebd0213c105f705cbe40aa96a
noportman <noah.portman@gmail.com>
2023-10-25 09:36:18 -0400

    Changed to personal links and logo

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,13 @@ st.set_page_config(layout="wide")
 
 # Customize the sidebar
 markdown = """
-Web App URL: <https://geotemplate.streamlit.app>
-GitHub Repository: <https://github.com/giswqs/streamlit-multipage-template>
+Web App URL: <https://noah-map.streamlit.app>
+GitHub Repository: <https://github.com/noportman/map-template>
 """
 
 st.sidebar.title("About")
 st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
+logo = "https://media.licdn.com/dms/image/D4E35AQFj0aysS6dqdQ/profile-framedphoto-shrink_400_400/0/1694531240649?e=1698847200&v=beta&t=yjABcii5ifUPj63oBUyECbqro0Jtj_WhxpOxnYFr4Rc"
 st.sidebar.image(logo)
 
 # Customize page title
